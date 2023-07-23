fbbc1148d08e61db00e3454b94b3785797d67f05
Matilda Mared <matildamared@live.se>
2023-07-23 08:33:54 +0200

    complete checkout exercise

--- src/app/exercises/02-checkout/page.js
+++ src/app/exercises/02-checkout/page.js
@@ -1,68 +1,69 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
-import DATA from './data';
-import reducer from './reducer';
-import StoreItem from './StoreItem';
-import CheckoutFlow from './CheckoutFlow';
-import './styles.css';
+import DATA from "./data";
+import reducer from "./reducer";
+import StoreItem from "./StoreItem";
+import CheckoutFlow from "./CheckoutFlow";
+import "./styles.css";
+import { set } from "date-fns";
 
 function CheckoutExercise() {
-  const [items, dispatch] = React.useReducer(
-    reducer,
-    null,
-    () => {
-      const savedItems =
-        window.localStorage.getItem('cart-items');
-
-      if (savedItems === null) {
-        return [];
-      }
-
-      return JSON.parse(savedItems);
-    }
-  );
-
+  const [items, dispatch] = React.useReducer(reducer, []);
+  const [isLoading, setIsLoading] = React.useState(true);
+  
   React.useEffect(() => {
-    window.localStorage.setItem(
-      'cart-items',
-      JSON.stringify(items)
-    );
-  }, [items]);
+		const savedItems = window.localStorage.getItem("cart-items");
 
-  return (
-    <>
-      <h1>Neighborhood Shop</h1>
+		if (savedItems === null) {
+			return;
+		}
 
-      <main>
-        <div className="items">
-          {DATA.map((item) => (
-            <StoreItem
-              key={item.id}
-              item={item}
-              handleAddToCart={(item) => {
-                dispatch({
-                  type: 'add-item',
-                  item,
-                });
-              }}
-            />
-          ))}
-        </div>
+    dispatch({
+      type: 'load-items',
+      items: JSON.parse(savedItems),
+    });
+    setIsLoading(false);
+  }, [])
+
+	React.useEffect(() => {
+		window.localStorage.setItem("cart-items", JSON.stringify(items));
+	}, [items]);
+
+	return (
+		<>
+			<h1>Neighborhood Shop</h1>
+
+			<main>
+				<div className="items">
+					{DATA.map((item) => (
+						<StoreItem
+							key={item.id}
+							item={item}
+							handleAddToCart={(item) => {
+								dispatch({
+									type: "add-item",
+									item,
+								});
+							}}
+						/>
+					))}
+				</div>
 
         <CheckoutFlow
-          items={items}
-          taxRate={0.15}
-          handleDeleteItem={(item) =>
-            dispatch({
-              type: 'delete-item',
-              item,
-            })
-          }
-        />
-      </main>
-    </>
-  );
+          loading={isLoading}
+					items={items}
+					taxRate={0.15}
+					handleDeleteItem={(item) =>
+						dispatch({
+							type: "delete-item",
+							item,
+						})
+					}
+				/>
+			</main>
+		</>
+	);
 }
 
 export default CheckoutExercise;

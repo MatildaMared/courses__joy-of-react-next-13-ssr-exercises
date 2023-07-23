491b48632754aa8b50f8d83a8388fb876ab3d9a3
Matilda Mared <matildamared@live.se>
2023-07-23 08:06:38 +0200

    solve clock exercise

--- src/app/exercises/01-clock/Clock.js
+++ src/app/exercises/01-clock/Clock.js
@@ -1,23 +1,21 @@
-'use client';
-import React from 'react';
-import format from 'date-fns/format';
+"use client";
+import React from "react";
+import format from "date-fns/format";
 
 function Clock() {
-  const [time, setTime] = React.useState(new Date());
+	const [time, setTime] = React.useState(null);
 
-  React.useEffect(() => {
-    const intervalId = window.setInterval(() => {
-      setTime(new Date());
-    }, 50);
+	React.useEffect(() => {
+		const intervalId = window.setInterval(() => {
+			setTime(new Date());
+		}, 50);
 
-    return () => {
-      window.clearInterval(intervalId);
-    };
-  }, []);
+		return () => {
+			window.clearInterval(intervalId);
+		};
+	}, []);
 
-  return (
-    <p className="clock">{format(time, 'hh:mm:ss.S a')}</p>
-  );
+	return <p className="clock">{time && format(time, "hh:mm:ss.S a")}</p>;
 }
 
 export default Clock;
